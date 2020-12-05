5c6e51dfd983de287c579883413c800ee5b1dc01
GraduaPro <71056587+GraduaPro@users.noreply.github.com>
2020-12-05 22:29:57 +0200

    Add files via upload

---
+++ Hand_tracking_landmarks.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  5 20:45:12 2020
+
+@author: eman-saeed
+"""
+
+import cv2
+import mediapipe as mp
+import tensorflow as tf
+from palm_detector import PalmDetector
+
+def Hand_Tracking():
+
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(
+    min_detection_confidence=0.7, min_tracking_confidence=0.5)
+    cap = cv2.VideoCapture(-1)
+    while True:
+        success, image = cap.read()
+        if not success:
+            break
+
+      # Flip the image horizontally for a later selfie-view display, and convert
+      # the BGR image to RGB.
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+      # To improve performance, optionally mark the image as not writeable to
+      # pass by reference.
+        image.flags.writeable = False
+        results = hands.process(image)
+
+      # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+              mp_drawing.draw_landmarks(
+                  image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+              cv2.imshow('MediaPipe Hands', image)
+              if cv2.waitKey(5) & 0xFF == 27:
+                  break
+    hands.close()
+    cap.release()
+
+Hand_Tracking()
